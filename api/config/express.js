edfb4da3eb9c3eb2e427b1955e1806887a1c74bc
raularaju <raularaju@uol.com.br>
2023-05-01 20:37:03 -0300

    ref: mudança na congif cors

--- api/config/express.js
+++ api/config/express.js
@@ -11,6 +11,15 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 app.use(express.static('public'));
+require('dotenv').config;
+
+const cors = require('cors');
+const corsOptions = {
+    origin: ['http://localhost:3030', 'http://localhost:5173', 'http://127.0.0.1:5173:3030', 'http://127.0.0.1:5173'],
+    credentials: true,            //access-control-allow-credentials:true
+    optionSuccessStatus: 200
+};
+app.use(cors(corsOptions));
 
 const UserRouter = require('../src/domains/User/controllers/index');
 app.use('/user', UserRouter);
