10e08cb02d6bfd73e4c20e051081031a72d52a72
raularaju <raularaju@uol.com.br>
2023-05-02 18:37:08 -0300

    ref: agora quando o back end está processando a atualização do produto os botões trocam para carregando

--- my-app/src/components/Products/ProductTable.tsx
+++ my-app/src/components/Products/ProductTable.tsx
@@ -1,17 +1,38 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
+import { updateQuantityProduct } from '../../requests/products';
 
 interface Product {
+  id: string
   name: string;
   brand: string;
-  expiration: string;
+  expirationDate: string;
   quantity: number;
 }
 
 interface ProductTableProps {
   products: Product[];
+  onQuantityUpdate: (productId: string, newQuantity: number) => void;
 }
 
-const ProductTable: FC<ProductTableProps> = ({ products }) => {
+const ProductTable: FC<ProductTableProps> = ({ products, onQuantityUpdate }) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const handleQuantityUpdate = (productId: string, event: React.ChangeEvent<HTMLInputElement>) => {
+    const newQuantity = parseInt(event.target.value, 10);
+    onQuantityUpdate(productId, newQuantity);
+  }
+
+  const handleQuantityUpdateButton = async (productId: string, quantity: number) => {
+    setIsLoading(true);
+    try {
+      await updateQuantityProduct(productId, quantity);
+      alert("Produto atualizado");
+    } catch (error) {
+      console.log("Erro:", error);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   return (
     <table>
       <thead>
@@ -20,15 +41,29 @@ const ProductTable: FC<ProductTableProps> = ({ products }) => {
           <th>Marca</th>
           <th>Validade</th>
           <th>Quantidade</th>
+          <th></th>
         </tr>
       </thead>
       <tbody>
-        {products.map((product, index) => (
-          <tr key={index}>
+        {products.map((product) => (
+          <tr key={product.id}>
             <td>{product.name}</td>
             <td>{product.brand}</td>
             <td>{new Date(product.expirationDate).toLocaleDateString()}</td>
-            <td>{product.quantity}</td>
+            <td>
+              <input
+                type="number"
+                min="1"
+                value={product.quantity}
+                onChange={(event) => handleQuantityUpdate(product.id, event)}
+              />
+            </td>
+            <td> {isLoading ? (
+                <div>Carregando...</div>
+              ) : (
+                <button onClick={() => handleQuantityUpdateButton(product.id, product.quantity)}>Atualizar</button>
+              )}
+            </td>
           </tr>
         ))}
       </tbody>
