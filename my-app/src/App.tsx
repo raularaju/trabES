aac8847b1f4ff3427f7203bebb6351edb8e0bf37
raularaju <raularaju@uol.com.br>
2023-05-02 15:34:39 -0300

    feat: tela de cadastro finalizada

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import LoginPage from './components/Login/LoginPage';
-import DashboardPage from './components/DashboardPage';
+import SignUpPage from './components/SignUp/SignUp';
 import ProductsPage from './components/Products/ProductsPage'
 import './App.css'
 function AppRoutes() {
@@ -12,6 +12,10 @@ function AppRoutes() {
           <Route
             path="/products"
             element={<ProductsPage />}
+          />
+          <Route
+            path="/signUp"
+            element={<SignUpPage />}
           />
         </Routes>
       </BrowserRouter>
---
+++ my-app/src/components/SignUp/SignUp.tsx
@@ -0,0 +1,206 @@
+    import React, { Component } from 'react';
+    import { createUser, signup } from '../../requests/user';
+    import { Link, useNavigate } from 'react-router-dom';
+    import isEmail from '../../../utils/isEmail'
+    import maskCPF from '../../../utils/maskCPF'
+    import TextMask from 'react-text-mask';
+    import isValidCPF from '../../../utils/isValidCPF'
+
+    import InputMask from "react-input-mask";
+/*     import './SignUp.css' */
+    interface Props {}
+    interface State {
+    name: string;
+    email: string;
+    password: string;
+    confirmPassword: string;
+    cpf: string;
+    phoneNumber: string;
+    isLoading: boolean;
+    errorMessage: string
+    isCPFbeingEdited: boolean
+    }
+
+    class SignUp extends Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+        this.state = {
+        name: '',
+        email: '',
+        password: '',
+        confirmPassword: '',
+        cpf: '',
+        phoneNumber: '',
+        isLoading: false,
+        errorMessage: '',
+        isCPFbeingEdited: false
+        };
+    }
+   
+    handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        this.setState({ errorMessage: '' });
+        const { name, email, password, confirmPassword,cpf, phoneNumber } = this.state;
+        
+        
+        if (name === '' || email === '' || password === '' || cpf === '' || phoneNumber === ''){
+            this.setState({ errorMessage: 'Preencha todos os campos' });
+            this.setState({isLoading: false})
+            return;
+        }
+
+        if(password !== confirmPassword){
+            this.setState({ errorMessage: 'Campos de senha e confirmação de senha não são iguais' });
+            this.setState({isLoading: false})
+            return;
+        }
+        
+        if (!isEmail(email)) {
+            this.setState({ errorMessage: 'Email inválido' });
+            this.setState({isLoading: false})
+            return;
+        }
+        this.setState({ isLoading: true });
+
+        if(!isValidCPF(cpf)){
+            this.setState({ errorMessage: 'CPF inválido' });
+            this.setState({isLoading: false})
+            return;
+        }
+        try {
+            const { name, email, password, cpf, phoneNumber } = this.state;
+            createUser(name, email, cpf, phoneNumber, password)
+            .then(()=>{
+                alert("Usuário adicionado ao sistema")
+                this.setState({
+                name: '',
+                email: '',
+                password: '',
+                cpf: '',
+                phoneNumber: '',
+                isLoading: false,
+                });
+
+            }).catch(() => {
+                this.setState({ errorMessage: 'Usuário já cadastrado' });
+                this.setState({ isLoading: false });
+            })
+        } catch (error) {
+            console.error(error);
+            this.setState({ errorMessage: 'Usuário já cadastrado' });
+            this.setState({ isLoading: false });
+        }
+    };
+
+    handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = event.target;
+        if (name === 'cpf'){
+            const maskedCPF = maskCPF(value)
+            this.setState({cpf:maskedCPF });
+        }
+        this.setState({ [name]: value } as Pick<State, keyof State>);
+    };
+    render() {
+        const {
+        name,
+        email,
+        password,
+        confirmPassword,
+        cpf,
+        phoneNumber,
+        isLoading,
+        errorMessage,
+        isCPFbeingEdited
+        } = this.state;
+    
+        return (
+        <div>
+            <h1>Sign Up</h1>
+            <form onSubmit={this.handleSubmit} noValidate>
+            <label htmlFor="name">Name:</label>
+            <input
+                type="text"
+                id="name"
+                name="name"
+                value={name}
+                onChange={this.handleChange}
+                required
+            />
+    
+            <label htmlFor="email">Email:</label>
+            <input
+                type="email"
+                id="email"
+                name="email"
+                value={email}
+                onChange={this.handleChange}
+                required
+            />    
+            <label htmlFor="password">Password:</label>
+            <input
+                type="password"
+                id="password"
+                name="password"
+                value={password}
+                onChange={this.handleChange}
+                required
+            />
+    
+            <label htmlFor="confirmPassword">Confirm Password:</label>
+            <input
+                type="password"
+                id="confirmPassword"
+                name="confirmPassword"
+                value={confirmPassword}
+                onChange={this.handleChange}
+                required
+            />
+    
+            <label htmlFor="cpf">CPF:</label>
+            <TextMask
+            mask={[
+              /\d/,
+              /\d/,
+              /\d/,
+              '.',
+              /\d/,
+              /\d/,
+              /\d/,
+              '.',
+              /\d/,
+              /\d/,
+              /\d/,
+              '-',
+              /\d/,
+              /\d/
+            ]}
+            placeholder="___.___.___-__"
+            id="cpf"
+            type="text"
+            name="cpf"
+            value={cpf}
+            onChange={this.handleChange}
+            />
+            <label htmlFor="phoneNumber">Phone Number:</label>
+            <InputMask
+                mask="(99) 99999-9999"
+                type="tel"
+                name="phoneNumber"
+                id="phoneNumber"
+                value={phoneNumber}
+                onChange={this.handleChange}
+            />
+            <button type="submit" disabled={isLoading}>
+                {isLoading ? "Loading..." : "Sign Up"}
+            </button>
+            {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
+            </form>
+            <h4>JÁ TEM UMA CONTA? <Link to='/'>FAÇA LOGIN</Link></h4>
+        </div>
+        
+        );
+    }
+    
+    }
+
+    export default SignUp;
